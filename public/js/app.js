400c1a0ca7bf8c8fc77d84879983c7990aacf718
Erwin Andres <erwinaandres@gmail.com>
2022-06-03 12:28:09 +0800

    Initial commit

---
+++ public/js/app.js
@@ -0,0 +1,25 @@
+const weatherForm = document.querySelector('form')
+const searchField = document.querySelector('input')
+const primaryParagraph = document.querySelector('#message-1')
+const secondaryParagraph = document.querySelector('#message-2')
+
+weatherForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  const address = searchField.value
+
+  primaryParagraph.textContent = 'Loading...'
+  secondaryParagraph.textContent = ''
+
+  fetch(`http://localhost:3000/weather?address=${ address }`).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        primaryParagraph.textContent = data.error
+        return
+      }
+
+      primaryParagraph.textContent = data.location
+      secondaryParagraph.textContent = data.forecast
+    })
+  })
+})
---
+++ src/app.js
@@ -0,0 +1,95 @@
+const path = require('path')
+const express = require('express')
+const res = require('express/lib/response')
+const hbs = require('hbs');
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+
+const app = express()
+
+const publicDir = path.join(__dirname, '../public')
+const viewsDir = path.join(__dirname, '../templates/views')
+const partialDir = path.join(__dirname, '../templates/partials')
+
+app.set('view engine', 'hbs')
+app.set('views', viewsDir)
+hbs.registerPartials(partialDir);
+
+app.use(express.static(publicDir))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        page: 'Home',
+        name: 'Erwin'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Weather App',
+        page: 'Help page',
+        helpText: 'Tulong! tulong! saklolo! kailangan ko ang tulong nyo!',
+        name: 'Erwin'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'Weather App',
+        page: 'About page',
+        name: 'Erwin'
+    })
+})
+
+app.get('/weather', (req, res) => {
+    if (!req.query.address) {
+        return res.send({
+            error: 'You must provide an address!'
+        })
+    }
+
+    geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+        if (error) {
+            return res.send({ error })
+        }
+
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.send({ error })
+            }
+
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            })
+        })
+    })
+})
+
+app.get('/products', (req, res) => {
+    if (!req.query.search) {
+        return res.send({
+            error: 'You must provide a search term!'
+        })
+    }
+
+    res.send({
+        products: []
+    })
+})
+
+app.get('/*', (req, res) => {
+    res.render('404', {
+        title: 'Weather App',
+        page: '404:',
+        name: 'Erwin'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000')
+})
+
+module.exports = app
